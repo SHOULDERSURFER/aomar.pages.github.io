0d8690cae9fbf099b1bdd7f6e66f8cc7aa9f42e2
Abd Omr <26618038+aaomr@users.noreply.github.com>
2024-07-01 00:56:43 +0800

    commit

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-const shareButtons = document.querySelectorAll('.tile-share-button')
-console.log(shareButtons)
-
-async function copyText(e) {
-//prevent button going to the site
-    e.preventDefault()
-    const link = this.getAttribute('link')
-    console.log(link)
-    try {
-        await navigator.clipboard.writeText(link)
-        alert("Copied the text: " + link)
-    } catch (err) {
-        console.error(err)
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Slick Carousel
+    $('.image-gallery').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+  
+    // Share buttons functionality
+    const shareButtons = document.querySelectorAll('.tile-share-button');
+    console.log(shareButtons);
+  
+    async function copyText(e) {
+      // Prevent button from going to the site
+      e.preventDefault();
+      const link = this.getAttribute('link');
+      console.log(link);
+      try {
+        await navigator.clipboard.writeText(link);
+        alert("Copied the text: " + link);
+      } catch (err) {
+        console.error(err);
+      }
     }
-}
-
-shareButtons.forEach(shareButton =>
-    shareButton.addEventListener('click', copyText))
-
-    $(document).ready(function(){
-        $('.image-gallery').slick({
-          dots: true,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 1,
-          adaptiveHeight: true
-        });
-      });
+  
+    shareButtons.forEach(button => button.addEventListener('click', copyText));
+  });
