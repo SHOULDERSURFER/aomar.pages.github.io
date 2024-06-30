1e8a26d784e330cec2f0ee33a6b0453f1c3db7de
Abd Omr <26618038+aaomr@users.noreply.github.com>
2024-07-01 01:02:24 +0800

    revert back

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Initialize Slick Carousel
-    $('.image-gallery').slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 1,
-      adaptiveHeight: true
-    });
-  
-    // Share buttons functionality
-    const shareButtons = document.querySelectorAll('.tile-share-button');
-    console.log(shareButtons);
-  
-    async function copyText(e) {
-      // Prevent button from going to the site
-      e.preventDefault();
-      const link = this.getAttribute('link');
-      console.log(link);
-      try {
-        await navigator.clipboard.writeText(link);
-        alert("Copied the text: " + link);
-      } catch (err) {
-        console.error(err);
-      }
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
     }
-  
-    shareButtons.forEach(button => button.addEventListener('click', copyText));
-  });
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
